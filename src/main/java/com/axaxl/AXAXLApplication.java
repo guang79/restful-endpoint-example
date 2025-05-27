6532dae1bb068e3d8a7f3297b98a01f684cb3d52
Henry Lin <guang79@yahoo.com>
2025-05-26 22:21:34 -0400

    add logger

--- src/main/java/com/axaxl/AXAXLApplication.java
+++ src/main/java/com/axaxl/AXAXLApplication.java
@@ -1,15 +1,18 @@
 package com.axaxl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AXAXLApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(AXAXLApplication.class);
+
     public static void main(String[] args) {
-
         SpringApplication.run(AXAXLApplication.class);
-
+        logger.info("AXAXLApplication started");
     }
 
 }
