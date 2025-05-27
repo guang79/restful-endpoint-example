60914c3511b8311d44845dc408efd57e2c707a7f
Henry Lin <guang79@yahoo.com>
2025-05-27 18:32:23 -0400

    clean up

--- src/test/java/com/axaxl/rest/DepartmentRestControllerTest.java
+++ src/test/java/com/axaxl/rest/DepartmentRestControllerTest.java
@@ -2,13 +2,10 @@ package com.axaxl.rest;
 
 import com.axaxl.entity.Department;
 import com.axaxl.repository.DepartmentRepository;
-import com.axaxl.configuration.ObjectMapperConfiguration;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -22,15 +19,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(DepartmentRestController.class)
 @AutoConfigureMockMvc
-@Import(ObjectMapperConfiguration.class)
 @ActiveProfiles("test")
 public class DepartmentRestControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-
-    @Autowired
-    private ObjectMapper objectMapper;
 
     @MockitoBean
     private DepartmentRepository departmentRepository;
--- src/test/java/com/axaxl/rest/EmployeeRestControllerTest.java
+++ src/test/java/com/axaxl/rest/EmployeeRestControllerTest.java
@@ -1,14 +1,11 @@
 package com.axaxl.rest;
 
-import com.axaxl.configuration.ObjectMapperConfiguration;
 import com.axaxl.entity.Employee;
 import com.axaxl.repository.EmployeeRepository;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -22,15 +19,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(EmployeeRestController.class)
 @AutoConfigureMockMvc
-@Import(ObjectMapperConfiguration.class)
 @ActiveProfiles("test")
 public class EmployeeRestControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-
-    @Autowired
-    private ObjectMapper objectMapper;
 
     @MockitoBean
     private EmployeeRepository employeeRepository;
